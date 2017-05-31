f2f8d21af0043b443f728b0985c034e1a959a684
Alan Vincent <alan.d.vincent@gmail.com>
2017-05-31 15:15:00 -0600

    added stuff

---
+++ app/controllers/people_controller.rb
@@ -0,0 +1,31 @@
+class PeopleController < ApplicationController
+
+
+ def index
+   @people = Person.all 
+  end
+
+  def show
+    @person = Person.find(params[:id])
+  end
+
+  def new
+    @person = Person.new
+  end
+
+  def create
+    @person = Person.new(person_params)
+
+    if @person.save
+      redirect_to people_path
+    else
+      render :new
+    end
+  end
+
+private
+
+def person_params
+  params.require(:person).permit(:name, :age, :hair_color, :eye_color, :gender, :alive)
+  end
+end
