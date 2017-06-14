f01aba24c49725844dc54d4edcbec2367b6ff5ea
Alan Vincent <alan.d.vincent@gmail.com>
2017-06-13 21:43:49 -0600

    more controller tests added

--- spec/controllers/people_spec.rb
+++ spec/controllers/people_spec.rb
@@ -70,9 +70,50 @@ RSpec.describe PeopleController, type: :controller do
             get :edit, params: { id: person.id }
             expect(response).to have_http_status(:success)
         end
+
+        it "sets the person instance variable" do
+              person = Person.create(name: "george", age:28, hair_color: "brown", 
+                                    eye_color: 'orange', gender: 'male', alive: true)
+            get :edit, params: { id: person.id }
+            expect(assigns(:person).id).to eq(person.id)
+        end
+
+        it "renders the edit template" do
+            person = Person.create(name: "george", age:28, hair_color: "brown", 
+                                    eye_color: 'orange', gender: 'male', alive: true)
+            get :edit, params: { id: person.id }
+            expect(response).to render_template(:edit)
+        end
     end
 
     describe "PUT #update" do
+        it "sets the person instance variable" do
+            person = Person.create(name: "george", age:28, hair_color: "brown", 
+                                    eye_color: 'orange', gender: 'male', alive: true)
+            put :update, params: { id: person.id, person: { eye_color: 'blue' } }
+            expect(assigns(:person)).to eq(person)
+        end
+
+        it "updates the person in the database" do
+            person = Person.create(name: "george", age:28, hair_color: "brown", 
+                                    eye_color: 'orange', gender: 'male', alive: true)
+            put :update, params: { id: person.id, person: { eye_color: 'blue' } }
+            expect(person.reload.eye_color).to eq('blue')
+        end
+
+        it "redirects to people path upon success" do
+             person = Person.create(name: "george", age:28, hair_color: "brown", 
+                                    eye_color: 'orange', gender: 'male', alive: true)
+            put :update, params: { id: person.id, person: { eye_color: 'blue' } }
+            expect(response).to redirect_to(people_path)
+        end
+
+        it "renders edit page if unsuccesful" do 
+             person = Person.create(name: "george", age:28, hair_color: "brown", 
+                                    eye_color: 'orange', gender: 'male', alive: true)
+            put :update, params: { id: person.id, person: { eye_color: '' } }
+            expect(response).to render_template(:edit)
+        end
      
     end
 
